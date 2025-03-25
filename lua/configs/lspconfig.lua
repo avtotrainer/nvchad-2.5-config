3bb46465f484fba30323c10368d5598b6d37ca05
avtotrainer <trainer@din.ge>
2025-03-25 20:18:21 +0400

    25.03.2025

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -6,7 +6,7 @@ local capabilities = configs.capabilities
 
 local lspconfig = require("lspconfig")
 
--- if you just want default config for the servers then put them in a table
+-- Existing servers
 local servers = { "html", "cssls", "ts_ls", "clangd", "gopls", "gradle_ls", "pyright", "ruff" }
 
 local function organize_imports()
@@ -39,3 +39,30 @@ for _, lsp in ipairs(servers) do
 	})
 	lspconfig.prismals.setup({})
 end
+
+-- Emmet Language Server
+lspconfig.emmet_language_server.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = {
+		"html",
+		"css",
+		"scss",
+		"sass",
+		"javascript",
+		"typescript",
+		"javascriptreact",
+		"typescriptreact",
+		"jsx",
+		"tsx",
+		"astro",
+		"vue",
+		"svelte",
+		"xml",
+	},
+	init_options = {
+		showAbbreviationSuggestions = true,
+		showExpandedAbbreviation = "always",
+		showSuggestionsAsSnippets = false,
+	},
+})
